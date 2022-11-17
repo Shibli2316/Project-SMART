2e2db43451a13409e7e30e768859b27a3b9d189e
Shibli <ahmedrazashibli@gmail.com>
2022-11-17 13:57:44 +0530

    Updating connection pages

---
+++ alumni/_dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+$server="localhost";
+$username="root";
+$password="";
+$databse="users_db";
+
+$conn=mysqli_connect($server, $username, $password, $databse);
+if (!$conn){
+    die("Error ". mysqli_connect_error());
+}
+else{
+    echo "The connnection was made"; 
+}
+?>
