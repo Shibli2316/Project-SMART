1a60b7815e08fc6e5015dd0109c4f71b5a47c10c
Shibli <ahmedrazashibli@gmail.com>
2022-11-19 20:15:38 +0530

    Login and Logout DONE

--- Sliding-Sign-In-Sign-Up-Form-master/welcome.php
+++ Sliding-Sign-In-Sign-Up-Form-master/welcome.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-
+include '../components/navcheck.php';
 echo "Welcome " .$_SESSION['username'];
 ?>
--- teacher/welcome.php
+++ teacher/welcome.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-
+include '../components/nav.php';
 echo "Welcome " .$_SESSION['username'];
 ?>
