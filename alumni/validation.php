d71b1c0ccbf6edd8966872009a490ae83c829a10
hamza10abc <hamza10abc@gmail.com>
2022-11-20 21:55:20 +0400

    SQLI PREVENTION

--- alumni/validation.php
+++ alumni/validation.php
@@ -6,6 +6,8 @@ include '_dbconnect.php';
 $username = $_POST['username'];
 $pass = $_POST['password'];
 
+$username = mysqli_real_escape_string($conn, $username);
+
 $sql = "SELECT * FROM alumni WHERE username = '$username'";
 $result = mysqli_query($conn, $sql);
 $num = mysqli_num_rows($result);
